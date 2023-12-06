842e1c6cef40df052f450da6bbcf5698688ff2be
Nathan Lugg <nathan.lugg@krakentechnologies.ltd>
2023-12-06 10:19:41 +0900

    add index_quantization()

--- src/fenix/vq/vq.py
+++ src/fenix/vq/vq.py
@@ -22,45 +22,65 @@ def distance(u: Tensor, v: Tensor, metric: str = "l2") -> Tensor:
 def build_quantization(
     x: Tensor,
     k: int = 256,
-    p: int = 1,
+    n: int = 1,
     f: int = 256,
     metric: str = "l2",
     epochs: int = 50,
 ) -> Tensor:
-    if p > 1 and metric == "cosine":
-        raise ValueError("Cannot use product quantization (p > 1) with 'cosine' metric")
-
-    x = x.view(x.size(0), p, x.size(-1) // p)
-    x = x.transpose(0, 1)
-
-    i = torch.randperm(x.size(1))[:k]
-    q = x[:, i]
+    i = torch.randperm(x.size(0))[:k]
+    q = x[i]
 
     for _ in range(epochs):
-        i = torch.randperm(x.size(1))[: k * f]
-        v = x[:, i]
+        i = torch.randperm(x.size(0))[: k * f]
+        v = x[i]
 
         d = distance(v, q, metric=metric)
 
-        i = torch.arange(p).unsqueeze(-1)
-        j = torch.argmin(d, dim=-1)
-        q = torch.index_put(q, (i, j), v, accumulate=True)
+        i = d.argmin(dim=-1)
+        q = q.index_put_((i,), v, accumulate=True)
 
-        n = j[:, None, :] == torch.arange(k)[None, :, None]
-        n = torch.sum(n, dim=-1, keepdim=True) + 1
+        c = i[None, :] == torch.arange(k)[:, None]
+        c = torch.sum(c, dim=-1, keepdim=True) + 1
 
-        q = q / n
+        q = q / c
+
+    q = torch.unsqueeze(q, dim=0)
+
+    if n > 1:
+        i = apply_quantization(x, q, metric=metric).squeeze(1)
+        x = x - q[0, i]
+
+        q = torch.concatenate(
+            [
+                q,
+                build_quantization(x, k, n - 1, f, metric, epochs),
+            ],
+            dim=0,
+        )
 
     return q
 
 
 def apply_quantization(x: Tensor, q: Tensor, metric: str = "l2") -> Tensor:
-    p = q.size(0)
-    k = q.size(1)
+    n = q.size(0)
 
-    x = x.view(x.size(0), p, x.size(-1) // p)
-    x = x.transpose(0, 1)
+    d = distance(x, q[0], metric=metric)
+    i = torch.argmin(d, dim=-1).unsqueeze(1)
 
-    i = torch.argmin(distance(x, q, metric=metric), dim=-1)
+    if n > 1:
+        r = x - q[0, i[0]]
+        i = torch.concatenate(
+            [
+                i,
+                apply_quantization(r, q[1:], metric=metric),
+            ],
+            dim=1,
+        )
 
-    return torch.arange(k**p).reshape(p * (k,))[i.unbind(0)]
+    return i
+
+
+def index_quantization(i: Tensor, k: int) -> Tensor:
+    n = i.size(1)
+    j = torch.arange(k**n).reshape(n * (k,))
+    return j[i.unbind(1)]
