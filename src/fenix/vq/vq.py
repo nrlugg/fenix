feb5fbaeeb095d601b127e2d19e80deae5f89a28
Nathan Lugg <nrlugg@gmail.com>
2023-12-13 12:59:21 +0900

    fix quantization

--- src/fenix/vq/vq.py
+++ src/fenix/vq/vq.py
@@ -36,20 +36,27 @@ def build_quantization(
         i = torch.randperm(x.size(0))[: k * f]
         v = x[i]
 
+        if metric == "cosine":
+            q = F.normalize(q, dim=-1)
+            v = F.normalize(v, dim=-1)
+
         d = distance(v, q, metric=metric)
 
         i = torch.argmin(d, dim=-1)
         q = torch.index_put_(q, (i,), v, accumulate=True)
 
-        c = i[None, :] == torch.arange(k)[:, None]
-        c = torch.sum(c, dim=-1, keepdim=True) + 1
+        if metric == "cosine":
+            q = F.normalize(q, dim=-1)
 
-        q = q / c
+        else:
+            c = i[None, :] == torch.arange(k)[:, None]
+            c = torch.sum(c, dim=-1, keepdim=True) + 1
+
+            q = q / c
 
     q = torch.unsqueeze(q, dim=0)
 
     if n > 1:
-        i = apply_quantization(x, q, metric=metric).squeeze(1)
         q = torch.concatenate(
             [
                 q,
@@ -66,23 +73,35 @@ def apply_quantization(x: Tensor, q: Tensor, metric: str = "l2") -> Tensor:
     q = q.dequantize()
     n = q.size(0)
 
-    d = distance(x, q[0], metric=metric)
-    i = torch.argmin(d, dim=-1).unsqueeze(1)
+    d = distance(x, q[0], metric=metric).unsqueeze(0)
 
     if n > 1:
-        r = x - q[0, i[0]]
-        i = torch.concatenate(
+        d = torch.concatenate(
             [
-                i,
-                apply_quantization(r, q[1:], metric=metric),
+                d,
+                apply_quantization(x, q[1:], metric=metric),
             ],
-            dim=1,
+            dim=0,
         )
 
-    return i
+    return d
 
 
-def index_quantization(i: Tensor, k: int) -> Tensor:
-    n = i.size(1)
-    j = torch.arange(k**n).reshape(n * (k,))
-    return j[i.unbind(1)]
+def index_quantization(d: Tensor, k: int, metric: str = "l2") -> Tensor:
+    n = d.size(0)
+
+    dim = (n - 1) * [None]
+    s = d[0, ..., *dim]
+
+    for i in range(1, n):
+        l = i * [None]
+        r = (n - i - 1) * [None]
+
+        s = s + d[i, :, *l, :, *r]
+
+    return torch.topk(
+        torch.flatten(s, start_dim=1),
+        k=k,
+        dim=-1,
+        largest=False,
+    ).indices
