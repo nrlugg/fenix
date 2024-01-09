5d28cb4df7e60be55748236224df6c5b71f6a8e5
Nathan Lugg <nathan.lugg@krakentechnologies.ltd>
2024-01-09 15:04:36 +0900

    completely revamp Dataset

--- src/fenix/ds/dataset.py
+++ src/fenix/ds/dataset.py
@@ -1,9 +1,8 @@
-import functools
 import os
 import uuid
 from dataclasses import dataclass
 from os.path import join
-from typing import Iterator, Sequence, TypedDict
+from typing import Iterator, NotRequired, Sequence, TypedDict
 
 import fsspec
 import numpy as np
@@ -19,96 +18,74 @@ import fenix.io as io
 import fenix.vq as vq
 
 BATCH_SIZE: int = 2**20
-TABLE_DIR: str = "data"
-INDEX_DIR: str = "indx"
-INDEX_COL: str = "__INDEX__"
-SCORE_COL: str = "__SCORE__"
+TABLE_PATH: str = "data"
+INDEX_PATH: str = "indx"
+INDEX_FILE: str = ".torch"
+INDEX_NAME: str = "__INDEX__"
+SCORE_NAME: str = "__SCORE__"
 
-Select = str | Sequence[str] | None
-Filter = pc.Expression | None
+
+class Index(TypedDict):
+    column: str
+    metric: str
+    version: NotRequired[str]
 
 
 class IndexConfig(TypedDict):
     k: int
     n: int
-    metric: str
     sample_size: int
     num_samples: int
 
 
-class SavedIndex(TypedDict):
+class IndexState(TypedDict):
     tensor: Tensor
-    column: str
     config: IndexConfig
 
 
-def index_field() -> pa.Field:
-    return pc.field(INDEX_COL)
+class IndexSpec(TypedDict):
+    index: Index
+    state: IndexState
 
 
-@dataclass
+def index_name(index: Index) -> str:
+    return join(index["column"], index["metric"], index["version"])
+
+
+def index_path(uri: str, index: Index) -> str:
+    return join(uri, INDEX_PATH, index_name(index))
+
+
+def index_data(uri: str, index: Index) -> IndexState:
+    with fsspec.open(join(index_path(uri, index), INDEX_FILE)) as f:
+        return torch.load(f, map_location="cpu")
+
+
+def index_topk(hash: str, index: Index) -> str:
+    return join("topk", hash, index_name(index))
+
+
+def index_top1(hash: str, index: Index) -> str:
+    return join("top1", hash, index_name(index))
+
+
+def index_dist(type: pa.DataType, index: Index) -> str:
+    return join(f"list_{index['metric']}_distance", str(type.value_type), str(type.list_size))
+
+
+@dataclass(frozen=True)
 class Dataset(ds.Dataset):
     uri: str
 
     def __post_init__(self) -> None:
-        self.filesystem.makedirs(self._table_uri(), exist_ok=True)
-        self.filesystem.makedirs(self._index_uri(), exist_ok=True)
+        fs = fsspec.filesystem("file")
+        fs.makedirs(join(self.uri, TABLE_PATH), exist_ok=True)
+        fs.makedirs(join(self.uri, INDEX_PATH), exist_ok=True)
 
-        for version, encoder in self.list_indexes().items():
-            self.register_encoder(version, encoder)
-            self.register_distance(encoder["column"], encoder["config"]["metric"])
-
-    def register_encoder(self, vers: int, data: SavedIndex) -> "Dataset":
-        name = self._index_encoder_name(vers)
-        type = self.schema.field(data["column"]).type
-
-        if name in pc.list_functions():
-            return self
-
-        def func(
-            ctx: pc.UdfContext, v: pa.FixedShapeTensorArray, k: pa.Int64Scalar
-        ) -> pa.ListArray:
-            x = vq.arrow_to_torch(v)
-            i = vq.encode(x, data["tensor"], k.as_py(), data["config"]["metric"])
-            return pa.array(iter(i.numpy()))
-
-        pc.register_scalar_function(
-            func,
-            name,
-            {"summary": "", "description": ""},
-            {"v": type, "k": pa.int64()},
-            pa.list_(pa.int64()),
-        )
-
-        return self
-
-    def register_distance(self, column: str, metric: str) -> "Dataset":
-        type = self.schema.field(column).type
-        size = type.shape[0]
-        name = f"list_{metric}_distance:{type.value_type}:{size}"
-
-        def dist(
-            ctx: pc.UdfContext, v: pa.FixedSizeListArray, q: pa.FixedSizeListScalar
-        ) -> pa.FloatArray:
-            return pa.array(
-                vq.distance(
-                    vq.arrow_to_torch(q).unsqueeze(0),
-                    vq.arrow_to_torch(v),
-                    metric=metric,
-                )
-                .squeeze(0)
-                .numpy()
-            )
-
-        pc.register_scalar_function(
-            dist,
-            name,
-            {"summary": "", "description": ""},
-            {"a": type, "b": type.storage_type},
-            type.value_type,
-        )
-
-        return self
+        for spec in self.list_indexes():
+            register_distance(self, spec["index"])
+            register_encoder(self, spec["index"])
+            self.update_index(spec["index"])
 
     @property
     def name(self) -> str:
@@ -120,50 +97,32 @@ class Dataset(ds.Dataset):
             os.path.abspath(self.uri).encode(),
         )
 
-    def _table_uri(self, name: str | None = None) -> str:
-        uri = join(self.uri, TABLE_DIR)
+    def to_table(self, index: Index | None = None) -> pa.Table:
+        t = io.arrow.from_ipc(
+            join(self.uri, TABLE_PATH),
+        )
 
-        if name is not None:
-            uri = join(uri, name)
-
-        return uri
-
-    def _index_uri(self, vers: int | None = None) -> str:
-        uri = join(self.uri, INDEX_DIR)
-
-        if vers is not None:
-            uri = join(uri, str(vers))
-
-        return uri
-
-    def _index_encoder_name(self, vers: int) -> str:
-        return f"encoder/{self.hash}/{vers}"
-
-    @functools.cached_property
-    def filesystem(self) -> fsspec.AbstractFileSystem:
-        return fsspec.filesystem("file")
-
-    def to_table(self, index: int | None = None) -> pa.Table:
-        t = io.arrow.from_ipc(self._table_uri())
-
-        if index is not None:
-            i = io.arrow.from_ipc(self._index_uri(index))
-            t = t.append_column(
-                INDEX_COL,
-                i.rename_columns([INDEX_COL]).column(INDEX_COL),
-            )
+        if index is not None and "version" in index:
+            i = io.arrow.from_ipc(index_path(self.uri, index))
+            t = t.append_column(INDEX_NAME, i.column(INDEX_NAME))
 
         return t
-
-    def to_pyarrow(self) -> ds.Dataset:
-        return ds.dataset(self.to_table())
-
-    def count_rows(self, filter: pc.Expression | None = None) -> int:
-        return self.to_pyarrow().count_rows(filter=filter)
 
     @property
     def schema(self) -> pa.Schema:
         return self.to_table().schema
+
+    def scanner(
+        self, columns: Sequence[str] | None = None, filter: pc.Expression | None = None
+    ) -> ds.Scanner:
+        return ds.Scanner.from_batches(
+            self.to_table().to_reader(BATCH_SIZE),
+            columns=columns,
+            filter=filter,
+        )
+
+    def count_rows(self, filter: pc.Expression | None = None) -> int:
+        return self.scanner().count_rows(filter=filter)
 
     def filter(self, expression: pc.Expression) -> "Dataset":
         raise NotImplementedError()
@@ -172,9 +131,12 @@ class Dataset(ds.Dataset):
         raise NotADirectoryError()
 
     def head(
-        self, num_rows: int, columns: Select = None, filter: pc.Expression | None = None
+        self,
+        num_rows: int,
+        columns: Sequence[str] | None = None,
+        filter: pc.Expression | None = None,
     ) -> pa.Table:
-        return self.to_pyarrow().head(num_rows, columns=columns, filter=filter)
+        return self.scanner().head(num_rows, columns=columns, filter=filter)
 
     def join(self, *args, **kwargs) -> "Dataset":
         raise NotADirectoryError()
@@ -182,16 +144,13 @@ class Dataset(ds.Dataset):
     def replace_schema(self, schema: pa.Schema) -> pa.Schema:
         raise NotADirectoryError()
 
-    def scanner(self, columns: Select = None, filter: pc.Expression | None = None) -> ds.Scanner:
-        return self.to_pyarrow().scanner(columns=columns, filter=filter)
-
     def sort_by(self, sorting: str | list[tuple[str, str]]) -> "Dataset":
         raise NotADirectoryError()
 
     def take(
         self,
         indices: Sequence[int] | np.ndarray | pa.Array,
-        columns: Select = None,
+        columns: Sequence[str] | None = None,
         filter: pc.Expression | None = None,
     ) -> pa.Array:
         if not isinstance(indices, np.ndarray):
@@ -206,7 +165,7 @@ class Dataset(ds.Dataset):
         t = self.to_table()
 
         if columns is not None:
-            t = t.Select(columns)
+            t = t.select(columns)
 
         t = t.filter(mask).take(indices)
 
@@ -216,14 +175,14 @@ class Dataset(ds.Dataset):
         return t
 
     def to_batches(
-        self, columns: Select = None, filter: pc.Expression | None = None
+        self, columns: Sequence[str] | None = None, filter: pc.Expression | None = None
     ) -> list[pa.RecordBatch]:
-        return self.to_pyarrow().to_batches(columns=columns, filter=filter)
+        return self.scanner().to_batches(columns=columns, filter=filter)
 
     def to_reader(
-        self, columns: Select = None, filter: pc.Expression | None = None
-    ) -> Iterator[pa.RecordBatch]:
-        return self.to_pyarrow().to_reader(columns=columns, filter=filter)
+        self, columns: Sequence[str] | None = None, filter: pc.Expression | None = None
+    ) -> pa.RecordBatchReader:
+        return self.scanner().to_reader(columns=columns, filter=filter)
 
     def sample(self, size: int) -> pa.Table:
         return self.to_table().filter(
@@ -234,89 +193,34 @@ class Dataset(ds.Dataset):
         if isinstance(data, pa.Table):
             data = data.to_reader(BATCH_SIZE)
 
-        path = self._table_uri(str(uuid.uuid4())) + ".arrow"
+        path = join(self.uri, TABLE_PATH, str(uuid.uuid4()) + ".arrow")
 
         io.arrow.to_ipc(path, data, data.schema)
 
-        return self.update_index()
+        return Dataset(self.uri)
 
-    def create_index(self, column: str, config: IndexConfig) -> "Dataset":
-        t = self.to_table()
-        v = t.column(column)
-        q = vq.kmeans(v, **config)
-
-        vers = max(0, *self.list_indexes(), 0) + 1
-        path = self._index_uri(vers)
-
-        self.filesystem.makedirs(path, exist_ok=True)
-
-        with self.filesystem.open(join(path, ".indx.torch"), "wb") as f:
-            data: SavedIndex = {"tensor": q, "column": column, "config": config}
-            torch.save(data, f)
-
-        return self.register_encoder(vers, data).update_index(vers)
-
-    def update_index(self, vers: int | None = None) -> "Dataset":
-        for version, encoder in self.list_indexes().items():
-            if vers is not None and version != vers:
-                continue
-
-            encoder_name = self._index_encoder_name(version)
-
-            for path in self.list_fragments():
-                data = io.arrow.from_ipc(path)
-                path = join(self._index_uri(version), os.path.basename(path))
-
-                if self.filesystem.exists(path):
-                    continue
-
-                indx = ac.from_sequence(
-                    ac.source(data),
-                    ac.select(
-                        **{
-                            INDEX_COL: ac.map(encoder_name, [ac.col(encoder["column"]), ac.lit(1)])
-                        },
-                    ),
-                    ac.select(
-                        **{
-                            INDEX_COL: ac.map("list_element", [ac.col(INDEX_COL), ac.lit(0)]),
-                        },
-                    ),
-                ).to_reader()
-
-                io.arrow.to_ipc(path, indx, indx.schema)
-
-        return self
-
-    def list_indexes(self) -> dict[int, SavedIndex]:
-        return {
-            vers: torch.load(join(path, ".indx.torch"))
-            for path in sorted(self.filesystem.glob(join(self.uri, INDEX_DIR, "*")))
-            for vers in [int(os.path.basename(path))]
-        }
-
-    def list_fragments(self) -> Iterator[str]:
-        yield from sorted(
-            self.filesystem.glob(join(self._table_uri(), "*.arrow")),
+    @property
+    def fragments(self) -> list[str]:
+        fs = fsspec.filesystem("file")
+        return sorted(
+            fs.glob(
+                join(self.uri, TABLE_PATH, "*.arrow"),
+            ),
         )
 
     def search_index(
         self,
-        query: pa.Array | pa.ChunkedArray | np.ndarray | Tensor,
-        index: int,
+        query: pa.Array | pa.ChunkedArray | pa.FixedSizeListScalar | np.ndarray | Tensor,
+        index: Index,
         limit: int,
         select: Sequence[str] | None = None,
         filter: pc.Expression | None = None,
-        probes: int = 8,
-    ) -> pa.RecordBatchReader:
-        encoder = self.list_indexes().get(index)
-        assert encoder is not None
+        probes: int | None = None,
+    ) -> pa.Table:
+        type = self.schema.field(index["column"]).type
+        dist = index_dist(type, index)
 
-        metric = encoder["config"]["metric"]
-        column = encoder["column"]
-        type = self.schema.field(column).type
-        size = type.shape[0]
-        func = f"list_{metric}_distance:{type.value_type}:{size}"
+        table = self.to_table(index)
 
         if isinstance(query, pa.ChunkedArray):
             query = query.combine_chunks()
@@ -324,33 +228,171 @@ class Dataset(ds.Dataset):
         if isinstance(query, pa.Array):
             query = query.to_numpy()
 
-        if isinstance(query, np.ndarray):
-            query = torch.from_numpy(query)
+        if isinstance(query, Tensor):
+            query = query.numpy()
 
-        source = self.to_table(index)
-        target = pa.scalar(query.numpy(), type=type.storage_type)
-        groups = pc.field(INDEX_COL).isin(
-            pa.array(
-                vq.encode(query, encoder["tensor"], probes, metric).squeeze().numpy(),
+        if isinstance(query, np.ndarray):
+            query = pa.scalar(query, type=type)
+
+        if "version" in index and probes is not None:
+            func = index_topk(self.hash, index)
+            mask = pc.field(INDEX_NAME).isin(
+                pc.call_function(func, [pa.array([query]), probes]).values,
             )
-        )
+
+            if filter is None:
+                filter = mask
+            else:
+                filter = filter & mask
 
         if select is None:
-            select = [
-                f.name for f in source.schema if not isinstance(f.type, pa.FixedShapeTensorType)
-            ]
+            select = [f.name for f in self.schema if not isinstance(f.type, pa.FixedSizeListType)]
 
-        if filter is None:
-            filter = groups
-        else:
-            filter = groups & filter
-
-        return ac.from_sequence(
-            ac.source(source),
+        reader = ac.from_sequence(
+            ac.source(table),
             ac.filter(filter),
             ac.select(
                 *select,
-                **{SCORE_COL: ac.map(func, [ac.col(column), target])},
+                **{SCORE_NAME: ac.map(dist, [ac.col(index["column"]), query])},
             ),
-            ac.order_by([(SCORE_COL, "ascending")]),
+            ac.order_by([(SCORE_NAME, "ascending")]),
         ).to_reader()
+
+        return ds.Scanner.from_batches(reader).take(np.arange(limit))
+
+    def create_index(self, index: Index, config: IndexConfig) -> "Dataset":
+        fs = fsspec.filesystem("file")
+
+        path = join(self.uri, INDEX_PATH, index["column"], index["metric"])
+
+        try:
+            vers = 1 + max(
+                [
+                    int(vers)
+                    for path in fs.ls(path)
+                    for vers in [os.path.basename(path)]
+                    if vers.isnumeric()
+                ],
+            )
+        except FileNotFoundError:
+            vers = 1
+
+        path = join(path, str(vers))
+
+        fs.makedirs(path, exist_ok=True)
+
+        v = self.to_table().column(index["column"])
+        q = vq.kmeans(v, metric=index["metric"], **config)
+
+        with fs.open(join(path, INDEX_FILE), "wb") as f:
+            torch.save({"tensor": q, "config": config}, f)
+
+        return Dataset(self.uri)
+
+    def update_index(self, index: Index) -> "Dataset":
+        fs = fsspec.filesystem("file")
+
+        func = index_top1(self.hash, index)
+
+        for path in self.fragments:
+            data = io.arrow.from_ipc(path)
+            path = join(index_path(self.uri, index), os.path.basename(path))
+
+            if fs.exists(path):
+                continue
+
+            indx = ac.from_sequence(
+                ac.source(data),
+                ac.select(
+                    **{INDEX_NAME: ac.map(func, [ac.col(index["column"])])},
+                ),
+            ).to_table()
+
+            io.arrow.to_ipc(path, indx, indx.schema)
+
+        return self
+
+    def list_indexes(self) -> Iterator[IndexSpec]:
+        fs = fsspec.filesystem("file")
+        for column_path in fs.ls(join(self.uri, INDEX_PATH)):
+            column = os.path.basename(column_path)
+
+            for metric_path in fs.ls(column_path):
+                metric = os.path.basename(metric_path)
+
+                for version_path in fs.ls(metric_path):
+                    version = os.path.basename(version_path)
+
+                    index: Index = {"column": column, "metric": metric, "version": version}
+                    state: IndexState = index_data(self.uri, index)
+
+                    yield {"index": index, "state": state}
+
+
+def register_encoder(dataset: Dataset, index: Index) -> None:
+    type = dataset.schema.field(index["column"]).type
+    name = index_topk(dataset.hash, index)
+
+    if name in pc.list_functions():
+        return
+
+    data = index_data(dataset.uri, index)
+
+    def topk(ctx: pc.UdfContext, v: pa.FixedSizeListType, k: pa.Int64Scalar) -> pa.ListType:
+        x = io.arrow.to_torch(v)
+
+        i = vq.encode(x, data["tensor"], k.as_py(), index["metric"]).numpy()
+        i = pa.ListArray.from_arrays(
+            pa.array(np.arange(0, i.size, k.as_py())),
+            pa.array(i.flatten()),
+        )
+
+        return i
+
+    def top1(ctx: pc.UdfContext, v: pa.FixedSizeListArray) -> pa.Int64Array:
+        return pc.list_element(topk(ctx, v, pa.scalar(1)), 0, memory_pool=ctx.memory_pool)
+
+    pc.register_scalar_function(
+        topk,
+        name,
+        {"summary": "", "description": ""},
+        {"v": type, "k": pa.int64()},
+        pa.list_(pa.int64()),
+    )
+
+    pc.register_scalar_function(
+        top1,
+        name.replace("topk", "top1"),
+        {"summary": "", "description": ""},
+        {"v": type},
+        pa.int64(),
+    )
+
+
+def register_distance(dataset: Dataset, index: Index) -> None:
+    type = dataset.schema.field(index["column"]).type
+    name = index_dist(type, index)
+
+    if name in pc.list_functions():
+        return None
+
+    def dist(
+        ctx: pc.UdfContext, v: pa.FixedSizeListType, q: pa.FixedSizeListType
+    ) -> pa.FloatArray:
+        return pa.array(
+            vq.distance(
+                io.arrow.to_torch(q).unsqueeze(0),
+                io.arrow.to_torch(v),
+                metric=index["metric"],
+            )
+            .squeeze(0)
+            .numpy()
+        )
+
+    pc.register_scalar_function(
+        dist,
+        name,
+        {"summary": "", "description": ""},
+        {"a": type, "b": type},
+        type.value_type,
+    )
