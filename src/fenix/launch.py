524544e9de8dad078eb906aeb077a9232adaf7a3
Nathan Lugg <nathan.lugg@krakentechnologies.ltd>
2024-01-11 09:47:36 +0900

    add launch

---
+++ src/fenix/launch.py
@@ -0,0 +1,21 @@
+import logging
+
+import typer
+
+from fenix.flight import Server
+
+logging.basicConfig()
+LOGGER = logging.getLogger("fenix")
+LOGGER.setLevel(level=logging.INFO)
+
+
+def launch(root: str, host: str = "0.0.0.0", port: int = 9001):
+    server = Server(root, host, port)
+
+    LOGGER.info(f" Server started at {server.grpc_location}")
+
+    server.serve()
+
+
+if __name__ == "__main__":
+    typer.run(launch)
